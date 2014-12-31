97373a780e8c3b34f8a9d1ada75d0dde436fe9c4
Dronny <dron09@nm.ru>
2014-12-31 06:07:31 +0300

    Update dmdload.py
    more

--- dmdload.py
+++ dmdload.py
@@ -2,13 +2,44 @@ def Load_F(filename):
     tmp_data = []
     with open(filename) as f:
         tmp_data = f.readlines()
-    marks = ['end', 'texture', 'models', 'file', 'new']
-    inited_marks = {}
-    for i in tmp_data:
-        for ii in marks:
-            if ii.capitalize() in i.capitalize():
-                inited_marks[ii] = tmp_data.index(i)
-    print(inited_marks)
+    class dmd_model:
+        class __dmd_mesh: vertnum = 0; facenum = 0; verts = []; faces = []; normals = []
+        class __dmd_material: diff_col = []; amb_col = []; spec_col = []; gloss = 0; opa = 0; diff = ""; bump = ""; spec = ""
+        class __dmd_texture: vertnum = 0; facenum = 0; verts = []; faces = []
+        mesh = __dmd_mesh(); material = __dmd_material(); texture = __dmd_texture()
+    model = dmd_model()
+    print("OK")
+    mesh_inited, mat_inited, texture_inited    = False, False, False
+    for i in range(len(tmp_data)):
+        if not mesh_inited      and "TriMesh"       in tmp_data[i]: mesh_inited = i
+        if not mat_inited       and "Material"      in tmp_data[i]: mat_inited = i
+        if not texture_inited   and "New Texture"   in tmp_data[i]: texture_inited = i
+    if mesh_inited:
+        model.mesh.vertnum, model.mesh.facenum = [int(i) for i in (tmp_data[mesh_inited + 2]).split()]
+        for i in range(model.mesh.vertnum):
+            model.mesh.verts.append(tuple([float(i) for i in (tmp_data[mesh_inited + 4 + i]).split()]))
+        for i in range(model.mesh.facenum):
+            model.mesh.faces.append(tuple([int(i) for i in (tmp_data[mesh_inited + 3 + model.mesh.vertnum + 3 + i]).split()]))
+    if mat_inited:
+        for i in (range(len(tmp_data)))[mat_inited:] :
+            if "diffuse color" in tmp_data[i]: model.material.diff_col = [int(i) for i in (tmp_data[i + 1]).split()]
+            if "ambient color" in tmp_data[i]: model.material.amb_col = [int(i) for i in (tmp_data[i + 1]).split()]
+            if "specular color" in tmp_data[i]: model.material.spec_col = [int(i) for i in (tmp_data[i + 1]).split()]
+            if "glossiness" in tmp_data[i]: model.material.gloss = int(tmp_data[i+1])
+            if "opacity" in tmp_data[i]: model.material.opa = int(tmp_data[i+1])
+            if "diffuse map" in tmp_data[i]: model.material.diff = tmp_data[i+1]
+            if "bump map" in tmp_data[i]: model.material.bump = tmp_data[i+1]
+            if "specular map" in tmp_data[i]: model.material.spec = tmp_data[i+1]
+            if "end material" in tmp_data[i]:  break
+    if texture_inited:
+        model.texture.vertnum, model.texture.facenum = [int(i) for i in (tmp_data[texture_inited + 2]).split()]
+        for i in range(model.texture.vertnum):
+            model.texture.verts.append(tuple( [float(i) for i in(tmp_data[texture_inited + 4 + i]).split()]))
+        for i in range(model.texture.facenum):
+            model.texture.faces.append(tuple([int(i) for i in (tmp_data[texture_inited + 3 + model.texture.vertnum + 3 + i]).split()]))
+    print(model.texture.faces)
+    return model
+            
 
 
-Load_F(r"someshithere")
+Load_F(r"C:\tmp\zdsim\routes\Шевченко-Пятихатки_2_0\models\tracks\1track.dmd")
